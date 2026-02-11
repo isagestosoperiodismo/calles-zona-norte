5686feabfc3acca2c87a1205ee5e0084997f83cf
Isabel Gestoso <isagestosocd@gmail.com>
2026-02-11 12:49:37 -0300

    h

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import process from "node:process";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    paths: {
+      base: process.argv.includes("dev") ? "" : "calles-zona-norte",
+    },
   },
 };
-
 export default config;
